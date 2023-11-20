afea7c81cbe98103e7aa3b258dd5ed343b87ec45
ShebDeaton <115922002+ShebDeaton@users.noreply.github.com>
2023-11-20 16:16:51 -0600

    Camera Controller for Tutorial

--- Adventure Game Collab/Assets/2D Game Testing/Assets/Scripts/CameraController.cs
+++ Adventure Game Collab/Assets/2D Game Testing/Assets/Scripts/CameraController.cs
@@ -21,15 +21,73 @@ public class CameraController : MonoBehaviour
         Debug.Log("Player Position: X = " + player.transform.position.x + " --- Y = " + player.transform.position.y + " --- Z = " +
         player.transform.position.z);
         Vector3 playerPosition = player.transform.position;
-        if (playerPosition.x > 9.5 && room == 4)
+        if (room == 4) // Main Room Navigation
         {
-            Camera.main.transform.Translate(18, 0, 0);
-            room = 5;
+            if (playerPosition.x > 9.5) // Enter Right Room
+            {
+                Camera.main.transform.Translate(18, 0, 0);
+                room = 5;
+            }
+            if (playerPosition.x < -9.5) // Enter Left Room
+            {
+                Camera.main.transform.Translate(-18, 0, 0);
+                room = 3;
+            }
+            if (playerPosition.y < -6) // Enter Bottom Room
+            {
+                Camera.main.transform.Translate(0, -10, 0);
+                room = 6;
+            }
+            if (playerPosition.y > 5) // Enter Top Room
+            {
+                Camera.main.transform.Translate(0, 10, 0);
+                room = 2;
+            }
         }
-        if (playerPosition.x < 8.5 && room == 5)
+        else if (room == 5) // Right Room Navigation / Movement Room
         {
-            Camera.main.transform.Translate(-18, 0, 0);
-            room = 4;
+            if (playerPosition.x < 8.5) // Enter Main Room
+            {
+                Camera.main.transform.Translate(-18, 0, 0);
+                room = 4;
+            }
+        }
+        else if (room == 3) // Left Room Navigation / Enemy Room
+        {
+            if (playerPosition.x > -8.5) // Enter Main Room
+            {
+                Camera.main.transform.Translate(18, 0, 0);
+                room = 4;
+            }
+        }
+        else if (room == 6) // Bottom Room Navigation / Shop Room
+        {
+            if (playerPosition.y > -5) // Enter Main Room
+            {
+                Camera.main.transform.Translate(0, 10, 0);
+                room = 4;
+            }
+        }
+        else if (room == 2) // Top Room Navigation / Boss Room
+        {
+            if (playerPosition.y < 4) // Enter Main Room
+            {
+                Camera.main.transform.Translate(0, -10, 0);
+                room = 4;
+            }
+            if (playerPosition.y > 15) // Enter Exit Room
+            {
+                Camera.main.transform.Translate(0, 10, 0);
+                room = 1;
+            }
+        }
+        else if (room == 1) // Top Top Room Navigation / Exit Room
+        {
+            if (playerPosition.y < 14) // Enter Boss Room
+            {
+                Camera.main.transform.Translate(0, -10, 0);
+                room = 2;
+            }
         }
     }
 }
