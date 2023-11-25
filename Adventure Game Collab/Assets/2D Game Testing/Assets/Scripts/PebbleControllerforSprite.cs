4e24e1777c9ccb5eec77045cd0195b8f0b7cd0bf
Landon2002 <81941270+Landon2002@users.noreply.github.com>
2023-11-25 10:46:34 -0600

    Dungeon Updated

---
+++ Adventure Game Collab/Assets/2D Game Testing/Assets/Scripts/PebbleControllerforSprite.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PebbleControllerforSprite : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public GameObject Money;
+    Rigidbody2D rb;
+    public int dir;
+    private Vector2 Direction1;
+    private Vector2 Direction2;
+    private Vector2 Direction3;
+    public int maxHealth = 10;
+    int currentHealth;
+    public float delay = 3.0f;
+    bool isInvincible;
+    float invincibleTimer;
+    public float timeInvincible = 0.25f;
+    public int health { get { return currentHealth; } }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        StartCoroutine(ShootProjectile());
+       
+        Direction1 = Vector2.left;
+        
+        Direction2 = Vector2.right;
+        
+        Direction3 = Vector2.down;
+        
+    }
+
+    void Update()
+    {
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
+
+        if (currentHealth == 0)
+        {
+            Destroy(gameObject);
+            Instantiate(Money, rb.position, Quaternion.identity);
+        }
+    }
+
+    IEnumerator ShootProjectile()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            Launch(Direction1);
+            Launch(Direction2);
+            Launch(Direction3);
+        }
+    }
+    void Launch(Vector2 Direction)
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rb.position + Direction * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(Direction, 150.0f);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
