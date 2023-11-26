fe1ba19a62c15ec3e9040fc488f50c9a478de5b9
ShebDeaton <115922002+ShebDeaton@users.noreply.github.com>
2023-11-26 04:34:46 -0600

    End Screen 95% Done
    Only Missing Credit Screen.
    In Tutorial Currently

--- Adventure Game Collab/Assets/2D Game Testing/Assets/Scripts/EndButtonManager.cs
+++ Adventure Game Collab/Assets/2D Game Testing/Assets/Scripts/EndButtonManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using static Cinemachine.DocumentationSortingAttribute;
 
 public class EndButtonManager : MonoBehaviour
 {
@@ -21,5 +23,24 @@ public class EndButtonManager : MonoBehaviour
                      "Damage Taken: " + MainManager.Instance.DamageTaken + "\n" +
                      "Projectiles Deflected: " + MainManager.Instance.ProjectilesDeflected;
         }
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void tryAgain()
+    {
+        if (MainManager.Instance != null)
+        {
+            MainManager.Instance.currentHealth = 10 + MainManager.Instance.level * MainManager.Instance.difficulty;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("UI Scene");
     }
 }
