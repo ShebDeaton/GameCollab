6aeaae60189a45ef4fc5e7d84b8a4786dad75c63
lrmanc <117472681+lrmanc@users.noreply.github.com>
2023-11-26 14:38:44 -0600

    Cave

--- Adventure Game Collab/Assets/2D Game Testing/Assets/Scripts/CaveCameraController.cs
+++ Adventure Game Collab/Assets/2D Game Testing/Assets/Scripts/CaveCameraController.cs
@@ -11,7 +11,7 @@ public class CaveCameraController : MonoBehaviour
     void Start()
     {
         player = GameObject.Find("Player");
-        room = 4;
+        room = 1; // Main Room 
 
     }
 
@@ -19,73 +19,74 @@ public class CaveCameraController : MonoBehaviour
     void Update()
     {
         Vector3 playerPosition = player.transform.position;
-        if (room == 4) // Main Room Navigation
+        if (room == 1) // Main Room Navigation
         {
             if (playerPosition.x > 9.5) // Enter Right Room
             {
                 Camera.main.transform.Translate(18, 0, 0);
-                room = 5;
+                room = 2;
             }
             if (playerPosition.x < -9.4) // Enter Left Room
             {
                 Camera.main.transform.Translate(-18, 0, 0);
                 room = 3;
             }
-            //if (playerPosition.y < -6) // Enter Bottom Room
-            //{
-                //Camera.main.transform.Translate(0, -10, 0);
-                //room = 6;
-            //}
             if (playerPosition.y > 5.1) // Enter Top Room
             {
                 Camera.main.transform.Translate(0, 10, 0);
-                room = 2;
+                room = 5;
             }
         }
-        else if (room == 5) // Right Room Navigation / Movement Room
+        else if (room == 2) // Right Room Navigation / Enemy Room
         {
             if (playerPosition.x < 8.3) // Enter Main Room
             {
                 Camera.main.transform.Translate(-18, 0, 0);
-                room = 4;
+                room = 1;
             }
         }
-        else if (room == 3) // Left Room Navigation / Enemy Room
+        else if (room == 3) // Left Room Navigation / Maze Room
         {
+            if (playerPosition.y > 5) // Enter Shop Room in Left Room
+            {
+                Camera.main.transform.Translate(0, 10, 0);
+                room = 4;
+            }
+
             if (playerPosition.x > -8.4) // Enter Main Room
             {
                 Camera.main.transform.Translate(18, 0, 0);
-                room = 4;
+                room = 1;
             }
         }
-        //else if (room == 6) // Bottom Room Navigation / Shop Room
-        //{
-            //if (playerPosition.y > -5) // Enter Main Room
-            //{
-                //Camera.main.transform.Translate(0, 10, 0);
-                //room = 4;
-            //}
-        //}
-        else if (room == 2) // Top Room Navigation / Boss Room
+        else if (room == 4) // Upper Left Room Navigation / Shop Room
+        {
+            if (playerPosition.y < 4) // Enter Left Room
+            {
+                Camera.main.transform.Translate(0, -10, 0);
+                room = 3;
+            }
+        }
+        else if (room == 5) // Top Room Navigation / Boss Room
         {
             if (playerPosition.y < 3.9) // Enter Main Room
             {
                 Camera.main.transform.Translate(0, -10, 0);
-                room = 4;
-            }
-            if (playerPosition.y > 15) // Enter Exit Room
-            {
-                Camera.main.transform.Translate(0, 10, 0);
                 room = 1;
             }
+            //if (playerPosition.y > 15) // Enter Exit Room
+            //{
+                //Camera.main.transform.Translate(0, 10, 0);
+                //room = 1;
+            //}
         }
-        else if (room == 1) // Top Top Room Navigation / Exit Room
-        {
-            if (playerPosition.y < 14) // Enter Boss Room
-            {
-                Camera.main.transform.Translate(0, -10, 0);
-                room = 2;
-            }
-        }
+        //else if (room == 1) // Top Top Room Navigation / Exit Room
+        //{
+            //if (playerPosition.y < 14) // Enter Boss Room
+            //{
+                //Camera.main.transform.Translate(0, -10, 0);
+                //room = 2;
+            //}
+        //}
     }
 }
